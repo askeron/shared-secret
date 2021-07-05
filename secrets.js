63bf9be42ac2102365f457e71e58e93711e492cc
Bjoern Kuhlen <bjoern@kuhlen.org>
2021-07-06 00:33:04 +0200

    commit

--- secrets.js
+++ secrets.js
@@ -1,14 +1,14 @@
 let secrets = [
     {
         "pseudonym": "05a0ab13",
-        "secret": "3746847cad12150d356dceae11fe3146 14a4b16c2ee8613335d9f861c80a3c91",
+        "secret": "3746847cad12150d356dceae11fe314614a4b16c2ee8613335d9f861c80a3c91",
     },
     {
         "pseudonym": "37caa23a",
-        "secret": "3746847cad12150d356dceae11fe3146 14a4b16c2ee8613335d9f861c80a3c92",
+        "secret": "3746847cad12150d356dceae11fe314614a4b16c2ee8613335d9f861c80a3c92",
     },
     {
         "pseudonym": "455e34dd",
-        "secret": "3746847cad12150d356dceae11fe3146 14a4b16c2ee8613335d9f861c80a3c93",
+        "secret": "3746847cad12150d356dceae11fe314614a4b16c2ee8613335d9f861c80a3c93",
     },
 ]
